3a8eb403c06d8e4cbaaa7aba4531572efc093098
ydying <1778679036@qq.com>
2019-07-10 18:47:23 +0800

    backup

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 const Home = resolve => require(['@/views/Home'], resolve)
-const About = resolve => require(['@/views/About'], resolve)
-const Help = resolve => require(['@/views/Help'], resolve)
 const World = resolve => require(['@/views/World'], resolve)
 const Error404 = resolve => require(['@/views/error/Error404'], resolve)
 
@@ -13,14 +11,6 @@ let routes = [
     {
         path: '/',
         component: Home
-    },
-    {
-        path: '/about',
-        component: About
-    },
-    {
-        path: '/help',
-        component: Help
     },
     {
         path: '/world',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,57 @@
 <template>
     <my-page title="像素图制作" :page="page">
-        <ui-row gutter>
-            <ui-col width="100" tablet="100" desktop="50">
-                <div class="btns">
-                    <ui-raised-button class="btn" label="下载" primary @click="download" />
-                    <ui-raised-button class="btn" label="填充" @click="fill" />
-                </div>
-                <input class="input" v-model="curColor" type="color">
-                <ul class="color-list">
-                    <!-- <li class="item item-current" 
-                        :style="{'background-color': curColor}"
-                        @click="selectColor(curColor)"></li> -->
-                    <li class="item" 
-                        :style="{'background-color': color}"
-                        @click="selectColor(color)"
-                        v-for="color in colors"></li>
-                </ul>
-            </ui-col>
-            <ui-col width="100" tablet="100" desktop="50">
-                <div v-if="edit">
-                    <input v-model="x">
-                    <input v-model="y">
-                </div>
-                <canvas id="canvas" width="448" height="448" 
-                    @mousedown="onMouseDown($event)"
-                    @mousemove="onMouseMove($event)"
-                    @mouseup="onMouseUp($event)"
-                    @mouseout="onMouseOut($event)"
-                    >
-                </canvas>
-                <!-- <ui-color-picker v-model="curColor" ></ui-color-picker> -->
-            </ui-col>
-        </ui-row>
-        
+        <div class="common-container container">
+            <ui-row gutter>
+                <ui-col width="100" tablet="100" desktop="50">
+                    <div class="btns">
+                        <ui-raised-button class="btn" label="下载" primary @click="download" />
+                        <ui-raised-button class="btn" label="填充" @click="fill" />
+                    </div>
+                    <input class="input" v-model="curColor" type="color">
+                    <ul class="color-list">
+                        <!-- <li class="item item-current"
+                            :style="{'background-color': curColor}"
+                            @click="selectColor(curColor)"></li> -->
+                        <li class="item"
+                            :style="{'background-color': color}"
+                            @click="selectColor(color)"
+                            v-for="color in colors"></li>
+                    </ul>
+                </ui-col>
+                <ui-col width="100" tablet="100" desktop="50">
+                    <div v-if="edit">
+                        <input v-model="x">
+                        <input v-model="y">
+                    </div>
+                    <canvas id="canvas" width="448" height="448"
+                        @mousedown="onMouseDown($event)"
+                        @mousemove="onMouseMove($event)"
+                        @mouseup="onMouseUp($event)"
+                        @mouseout="onMouseOut($event)"
+                        @touchstart="onTouchStart($event)"
+                        @touchmove="onTouchMove($event)"
+                        @touchend="onTouchEnd($event)"
+                        >
+                    </canvas>
+                    <!-- <ui-color-picker v-model="curColor" ></ui-color-picker> -->
+                </ui-col>
+            </ui-row>
+        </div>
+        <ui-drawer class="settingBox" :open="settingVisible" :docked="false" right @close="toggleSetting()">
+            <ui-appbar title="设置">
+                <ui-icon-button icon="close" slot="left" @click="toggleSeitting" />
+                <ui-icon-button icon="check" slot="right" @click="finish" />
+            </ui-appbar>
+            <div class="body">
+                <!-- 设置内容 -->
+                <ui-text-field v-model.number="gridNum" type="number" label="格子数量" />
+            </div>
+        </ui-drawer>
     </my-page>
 </template>
 
 <script>
     const saveAs = window.saveAs
-    // let grid = 28
-    // let SIZE = 16
-    let grid = 14
-    let SIZE = 32
 
     export default {
         data () {
@@ -49,13 +59,23 @@
                 colors: ['#000000', '#ffffff', '#ff0000', '#0099cc'],
                 curGrid: null,
                 curColor: '#000000',
+                settingVisible: false,
+                gridNum: 8,
+                gridSize: 14,
                 map: [],
                 page: {
                     menu: [
                         {
                             type: 'icon',
-                            icon: 'help',
-                            to: '/help'
+                            icon: 'settings',
+                            click: this.toggleSetting,
+                        },
+                        {
+                            type: 'icon',
+                            icon: 'apps',
+                            href: 'https://app.yunser.com?utm_source=pixel',
+                            target: '_blank',
+                            title: '应用'
                         }
                     ]
                 },
@@ -65,29 +85,24 @@
             }
         },
         mounted() {
-            console.log(this.$route.params)
-            if (this.$route.query.edit) {
-                this.edit = true
-                this.x = this.$route.query.x
-                this.y = this.$route.query.y
-            }
-            for (let row = 0; row < SIZE; row++) {
-                let rowData = []
-                for (let column = 0; column < SIZE; column++) {
-                    // rowData.push(null)
-                    rowData.push('#fff')
-                }
-                this.map.push(rowData)
-            }
-            // this.map[2][3] = '#09c'
-            // this.map[4][4] = 'f00'
-
             this.canvas = document.getElementById('canvas')
-            this.canvas.style.width = '448px'
-            this.canvas.style.height = '448px'
+            let size = window.innerWidth - 32
+            if (size > 480) {
+                size = 480
+            }
+            this.size = size
+            this.canvas.style.width = size + 'px'
+            this.canvas.style.height = size + 'px'
             this.ctx = this.canvas.getContext('2d')
             this.ctx.width = 448
             this.ctx.height = 448
+
+            this.initData()
+
+            // this.map[2][3] = '#09c'
+            // this.map[4][4] = 'f00'
+
+
             // this.ctx.fillRect(0, 0, 100, 100)
             this.timer = setInterval(this.update, 100)
         },
@@ -95,6 +110,46 @@
             clearInterval(this.timer)
         },
         methods: {
+            initData() {
+                this.gridSize = 448 / this.gridNum
+                this.map = []
+
+                console.log(this.$route.params)
+                if (this.$route.query.edit) {
+                    this.edit = true
+                    this.x = this.$route.query.x
+                    this.y = this.$route.query.y
+                }
+                for (let row = 0; row < this.gridNum; row++) {
+                    let rowData = []
+                    for (let column = 0; column < this.gridNum; column++) {
+                        // rowData.push(null)
+                        rowData.push('#fff')
+                    }
+                    this.map.push(rowData)
+                }
+            },
+            toggleSetting() {
+                this.settingVisible = !this.settingVisible
+            },
+            finish() {
+                this.settingVisible = false
+                if (!this.gridNum) {
+                    this.$message({
+                        type: 'danger',
+                        text: '请输入格子数量'
+                    })
+                    return
+                }
+                if (this.gridNum < 1) {
+                    this.$message({
+                        type: 'danger',
+                        text: '格子数量至少为 2'
+                    })
+                    return
+                }
+                this.initData()
+            },
             update() {
                 this.ctx.clearRect(0, 0, 448, 448)
                 this.ctx.strokeStyle = '#666'
@@ -111,34 +166,49 @@
                 //     this.ctx.stroke()
                 // }
 
-                for (let row = 0; row < SIZE; row++) {
-                    for (let column = 0; column < SIZE; column++) {
+                for (let row = 0; row < this.gridNum; row++) {
+                    for (let column = 0; column < this.gridNum; column++) {
                         if (this.map[row][column]) {
                             this.ctx.fillStyle = this.map[row][column]
-                            this.ctx.fillRect(row * grid, column * grid, grid, grid)
+                            this.ctx.fillRect(row * this.gridSize, column * this.gridSize, this.gridSize, this.gridSize)
                         }
                     }
                 }
                 if (this.curGrid) {
                     this.ctx.fillStyle = this.curColor
-                    this.ctx.fillRect(this.curGrid.x * grid, this.curGrid.y * grid, grid, grid)
+                    this.ctx.fillRect(this.curGrid.x * this.gridSize, this.curGrid.y * this.gridSize, this.gridSize, this.gridSize)
                 }
             },
             onMouseDown(e) {
                 if (e.button === 2) {
                     return
                 }
+                this._start(e)
+            },
+            onTouchStart(e) {
+                console.log('start', e)
+                this._start(e.touches[0])
+            },
+            _start(pt) {
                 let rect = this.canvas.getBoundingClientRect()
-                let x = parseInt((e.pageX - rect.x) / grid)
-                let y = parseInt((e.pageY - rect.y) / grid)
+                let x = parseInt(((pt.pageX - rect.x) / this.size * 448) / this.gridSize)
+                let y = parseInt(((pt.pageY - rect.y) / this.size * 448) / this.gridSize)
+
+
                 this.map[x][y] = this.curColor
                 this.isDown = true
                 this.addColor(this.curColor)
             },
             onMouseMove(e) {
+                this._move(e)
+            },
+            onTouchMove(e) {
+                this._move(e.touches[0])
+            },
+            _move(pt) {
                 let rect = this.canvas.getBoundingClientRect()
-                let x = parseInt((e.pageX - rect.x) / grid)
-                let y = parseInt((e.pageY - rect.y) / grid)
+                let x = parseInt(((pt.pageX - rect.x) / this.size * 448) / this.gridSize)
+                let y = parseInt(((pt.pageY - rect.y) / this.size * 448) / this.gridSize)
                 this.curGrid = {
                     x: x,
                     y: y
@@ -151,6 +221,12 @@
                 // let rect = this.canvas.getBoundingClientRect()
                 // let x = parseInt((e.pageX - rect.x) / grid)
                 // let y = parseInt((e.pageY - rect.y) / grid)
+                this._up()
+            },
+            onTouchEnd(e) {
+                this._up()
+            },
+            _up() {
                 this.isDown = false
             },
             onMouseOut(e) {
@@ -169,8 +245,8 @@
                 this.curColor = color
             },
             fill() {
-                for (let row = 0; row < SIZE; row++) {
-                    for (let column = 0; column < SIZE; column++) {
+                for (let row = 0; row < this.gridNum; row++) {
+                    for (let column = 0; column < this.gridNum; column++) {
                         // rowData.push(null)
                         this.map[row][column] = this.curColor
                     }
@@ -187,6 +263,13 @@
 </script>
 
 <style lang="scss" scoped>
+.settingBox {
+    width: 100%;
+    max-width: 320px;
+    .body {
+        padding: 16px;
+    }
+}
 canvas {
     margin-bottom: 16px;
     background-color: #fff;
